0b955b3ee6dbbda4ef4189814eb8cb250ea5f7a5
Duclearc <59476105+Duclearc@users.noreply.github.com>
2020-10-04 19:31:27 +0200

    alert shows when api connection fails. Console shows if key is missing

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 module.exports = (query, page) => {
-    const API_URL = `https://pixabay.com/api/?key=${process.env.VUE_APP_API_KEY.replace(
-        "_",
-        "-"
-    )}&q=${query.replace(/ /g, "+")}&page=${page || 1}`;
-    return fetch(API_URL).then(res => res.json())
+    const API_KEY = process.env.VUE_APP_API_KEY?.replace("_","-") || '';
+    const API_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query.replace(/ /g, "+")}&page=${page || 1}`;
+    return fetch(API_URL)
+        .then(res => res.json())
+        .catch(err => {
+            const errMsg = API_KEY ? '':'API KEY MISSING';
+            console.log(err, errMsg);
+            alert('Could not connect to server. Please reload and try again');
+        })
 }
