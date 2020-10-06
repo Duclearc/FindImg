806d61a64e99dd83a906cf128a1c29f642b8254d
Duclearc <59476105+Duclearc@users.noreply.github.com>
2020-10-06 19:05:43 +0200

    fixed fetch cors error

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 module.exports = (query, page) => {
     const API_KEY = process.env.VUE_APP_API_KEY?.replace("_","-") || '';
     const API_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${query.replace(/ /g, "+")}&page=${page || 1}`;
-    return fetch(API_URL)
+    return fetch(API_URL, {
+        mode: 'no-cors'
+    })
         .then(res => res.json())
         .catch(err => {
             const errMsg = API_KEY ? '':'API KEY MISSING';
